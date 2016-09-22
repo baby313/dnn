fix softmax

layer/fc.py
@@ -13,14 +13,16 @@ class Fc():
 		return self.output
 	def backward(self):
 		self.delta *= algorithm.sigmoid_gradient(self.output)
-		self.weight_update = np.dot(np.transpose(self.delta), self.prev.output)
+		for i in self.delta:
+			self.bias_update += i
+		self.weight_update += np.dot(np.transpose(self.delta), self.output)
 		self.prev.delta = self.delta * self.weight
 	def update(self):
-		self.bias += self.bias_update
 		self.weight_update -= self.weight * algorithm.decay * config.batch
 		self.weight += self.weight_update * algorithm.learning_rate / config.batch
-		self.bias_update *= algorithm.momentum
 		self.weight_update *= algorithm.momentum
+		self.bias += self.bias_update
+		self.bias_update *= algorithm.momentum
 	def connected(self, prev):
 		self.weight = np.random.rand(prev.outputs, self.outputs)
 		self.weight_update = np.zeros((prev.outputs, self.outputs))

layer/pool.py
@@ -8,7 +8,7 @@ class MaxPool():
 		self.delta = None
 	def forward(self):
 		input = self.prev.output.reshape(config.batch, self.c, self.h, self.w)
-		self.output = np.zeros((config.batch, self.c, self.h / self.step, self.w / self.step))
+		self.output = np.zeros((config.batch, self.c, self.h // self.step, self.w // self.step))
 		out_w, out_h, c = self.output_size()
 		for b in range(config.batch):
 			for c in range(self.c):