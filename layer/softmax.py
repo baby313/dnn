fix softmax update

layer/softmax.py
@@ -10,18 +10,21 @@ class Softmax():
 		self.prob = x / x.sum(axis=1).reshape(config.batch, -1)
 		self.result = self.prob.argmax(axis=1)
 	def backward(self):
-		truth_bool = np.zeros(self.prev.output.shape)
-		for i in range(config.batch):
-			truth_bool[i, self.truth[i]] = 1
-		self.prev.delta = (truth_bool - self.prev.output).transpose()
-	def update(self):
 		prob_bool = np.zeros(self.prob.shape)
 		for i in range(config.batch):
 			prob_bool[i, self.truth[i]] = 1
 		diff = prob_bool - self.prob
 		for i in range(self.group):
-			self.theta_update[:, i] += (self.input * diff[:,i]).sum(axis=0).transpose()
-		self.theta_update -= self.theta * algorithm.decay * config.batch
+			sum = np.zeros(self.theta.shape[0])
+			for b in range(config.batch):
+				sum += self.input[b] * diff[b, i]
+			self.theta_update[:, i] = sum
+		truth_bool = np.zeros(self.prev.output.shape)
+		for i in range(config.batch):
+			truth_bool[i, self.truth[i]] = 1
+		self.prev.delta = (truth_bool - self.prev.output).transpose()
+	def update(self):
+		self.theta_update -= self.theta * config.decay * config.batch
 		self.theta += config.learn_rate / config.batch * self.theta_update
 		self.theta_update *= config.momentum
 	def connected(self, prev):